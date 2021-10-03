fcec5a97ab1f2dcf04e1f17fa8b85c57d6fb1113
Justiceserv <justiceserv@jserv.xyz>
2021-10-03 14:14:44 +0900

    Why is this not working?

--- PC_Client/index.js
+++ PC_Client/index.js
@@ -1,1 +1,57 @@
-const iTunes = require('itunes-bridge')
+const iTunes = require('itunes-bridge');
+const request = require('request')
+const configuration = require('./datas/config.json')
+const iTunesEmitter = iTunes.emitter; 
+
+iTunesEmitter.on('playing', function (type, CurrentTrack) {
+    var options = {
+        uri: `http://${configuration.serverip}:${configuration.serverport}/newrpc`, 
+        method: 'POST', 
+        json: {
+            "name" : CurrentTrack.name,
+            "album" : CurrentTrack.album,
+            "artist" : CurrentTrack.artist,
+            "genre" : CurrentTrack.genre, 
+            "remainingTime" : CurrentTrack.remainingTime, 
+            "secretKey" : configuration.secretpass
+        }
+    };
+    request(options, function (error, response, body) {
+        if(!(!error) || response.statusCode != 200) {
+            console.log(body)
+            console.log(error)
+        }
+    });
+})
+
+iTunesEmitter.on('paused', function () { 
+    var options = {
+        uri: `http://${configuration.serverip}:${configuration.serverport}/pauserpc`, 
+        method: 'POST', 
+        json: {
+            "secretKey" : configuration.secretpass
+        }
+    };
+    request(options, function (error, response, body) {
+        if(!(!error) || response.statusCode != 200) {
+            console.log(body)
+            console.log(error)
+        }
+    });
+})
+
+iTunesEmitter.on('stopped', function () { 
+    var options = {
+        uri: `http://${configuration.serverip}:${configuration.serverport}/pauserpc`, 
+        method: 'POST', 
+        json: {
+            "secretKey" : configuration.secretpass
+        }
+    };
+    request(options, function (error, response, body) {
+        if(!(!error) || response.statusCode != 200) {
+            console.log(body)
+            console.log(error)
+        }
+    });
+})
